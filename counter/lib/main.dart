389d8aefc69c748b4bc6fc70a711ecab04321b19
Caio Cesar <caio.sfg@gmail.com>
2021-08-27 00:02:40 -0300

    inicio da parte de contagem

---
+++ counter/lib/main.dart
@@ -0,0 +1,24 @@
+void main(){
+  var galao = modeloGalao();
+  chooseWatter();
+}
+
+void chooseWatter(){
+ String produto;
+  int quantidade;
+  double preco;
+
+  // REGISTRO DO PRODUTO
+  produto = "Galão Cascataí";
+  quantidade = 20;
+  preco = 12.99;
+
+  print("Estoque no momento");
+  print(produto);
+  print(quantidade);
+  print(preco);
+}
+
+class modeloGalao {
+  //53
+}
