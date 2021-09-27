d6ae67b561cd37eaed39b54060b5e638ab387c5f
caiosfg <caio.sfg@gmail.com>
2021-09-27 18:10:15 -0300

    ajustes para pwa

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,17 @@
 <template>
-  <div id="app">
-    <BaseHeader />
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import BaseHeader from './components/BaseHeader.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
-    BaseHeader
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -27,5 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
